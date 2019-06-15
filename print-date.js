1abef1172fdde1ccaaa729529d5e309465be3ecb
Llombe_The_Last_King <lomskabs@gmail.com>
2019-06-15 02:58:57 +0100

    Current Time Function

--- print-date.js
+++ print-date.js
@@ -1,2 +1,22 @@
 // add a printDate function in here
 // eg: console.log("Hello World")
+<html>
+<head>
+<script>
+function printDate() {
+  document.getElementById("demo").innerHTML = Date()
+}
+console.log(printDate());
+</script>
+</head>
+<body>
+
+<h2>Current Time</h2>
+
+<p id="demo"></p>
+
+<button type="button" onclick="printDate()">Click to Check Time</button>
+
+
+</body>
+</html>
