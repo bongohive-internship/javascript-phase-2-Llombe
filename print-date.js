fcfcaa410264272ddf055710a0a60b2c1dd25d53
Llombe_The_Last_King <lomskabs@gmail.com>
2019-06-15 03:02:14 +0100

    Update print-date.js

--- print-date.js
+++ print-date.js
@@ -6,17 +6,18 @@
 function printDate() {
   document.getElementById("demo").innerHTML = Date()
 }
+
 console.log(printDate());
+
 </script>
 </head>
 <body>
 
 <h2>Current Time</h2>
 
-<p id="demo"></p>
+<p id = "demo"></p>
 
-<button type="button" onclick="printDate()">Click to Check Time</button>
-
+<button type = "button" onclick = "printDate()">Click to Check Time</button>
 
 </body>
 </html>
