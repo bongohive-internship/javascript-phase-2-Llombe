5a6789ef666d4a00f19a54b07e0974633d871a14
Llombe_The_Last_King <lomskabs@gmail.com>
2019-06-15 15:13:25 +0100

    update tell-fortune.js

--- tell-fortune.js
+++ tell-fortune.js
@@ -2,7 +2,10 @@
 <html>
     <body>
         <script>
-            
+            function tellFortune (str1, str2, str3, str4) {
+                alert ("tell fortune:" +arguments[0] +" "+ arguments[1]+" "+arguments[2]+" "+arguments[3])
+            }
+            tellFortune("number of children:", "partner's name:", "geographic location: ", "job title: ");
         </script>
     </body>
 </html>
